a53b306ab2c3d07b0500d4f820029fa34ac59789
Firmino Changani <firmino.changani@gmail.com>
2021-08-31 22:47:48 +0100

    chore: setup axios' default url

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import App from "./App";
 import React from "react";
+import axios from "axios";
 import store from "./store";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+
+axios.defaults.baseURL = process.env.REACT_APP_COINGECKO_API;
 
 ReactDOM.render(
   <Provider store={store}>
