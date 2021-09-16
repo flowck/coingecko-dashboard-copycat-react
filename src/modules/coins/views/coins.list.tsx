60033e15db462845ea0d32428f255ee4d2559ced
Firmino Changani <firmino.changani@gmail.com>
2021-09-16 15:06:52 +0100

    refactor(#8): replace connect() with hooks
    - addopt useSelector to map state
    - addopt useDispatch to dispatch actions

--- src/modules/coins/views/coins.list.tsx
+++ src/modules/coins/views/coins.list.tsx
@@ -1,41 +1,35 @@
-import { Action } from "redux";
-import { connect } from "react-redux";
 import styled from "styled-components";
 import { RootState } from "@store/index";
-import { ThunkDispatch } from "redux-thunk";
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { ViewTitle } from "@common/components/viewTitle/viewTitle";
 import { Coin, CoinsCategory } from "@coins/store/coins.interfaces";
 import { CoinsTable } from "@coins/components/coinsTable/coinsTable";
 import { Categories } from "@coins/components/categories/categories";
-import { ViewTitle } from "@common/components/viewTitle/viewTitle";
 import { fetchCoinsCategories, fetchCoinsPerMarket } from "../store/coins.thunks";
-
-interface CoinsListProps {
-  coins: Coin[];
-  fetchCategories(): void;
-  categories: CoinsCategory[];
-  fetchCoins(vsCurrency: string, page?: number, category?: string): void;
-}
 
 const FiltersContainer = styled.div`
   margin-bottom: 20px;
 `;
 
-function CoinsList({ fetchCoins, fetchCategories, categories, coins }: CoinsListProps) {
+function CoinsList() {
+  const dispatch = useDispatch();
   const [vsCurrency] = useState("usd");
   const [selectedCategory, setSelectedCategory] = useState("");
+  const coins = useSelector<RootState, Coin[]>(({ coinsModule }) => coinsModule.coins);
+  const categories = useSelector<RootState, CoinsCategory[]>(({ coinsModule }) => coinsModule.categories);
 
   useEffect(() => {
-    fetchCoins(vsCurrency);
-    fetchCategories();
-  }, [fetchCoins, fetchCategories, vsCurrency]);
+    dispatch(fetchCoinsPerMarket(vsCurrency));
+    dispatch(fetchCoinsCategories());
+  }, [vsCurrency, dispatch]);
 
   useEffect(() => {
-    fetchCoins(vsCurrency, 1, selectedCategory);
-  }, [selectedCategory, fetchCoins, vsCurrency]);
+    dispatch(fetchCoinsPerMarket(vsCurrency, 1, selectedCategory));
+  }, [selectedCategory, vsCurrency, dispatch]);
 
   const paginationHandler = (page: number) => {
-    fetchCoins(vsCurrency, page, selectedCategory);
+    dispatch(fetchCoinsPerMarket(vsCurrency, page, selectedCategory));
   };
 
   return (
@@ -51,19 +45,4 @@ function CoinsList({ fetchCoins, fetchCategories, categories, coins }: CoinsList
   );
 }
 
-function mapStateToProps(state: RootState) {
-  return {
-    coins: state.coinsModule.coins,
-    categories: state.coinsModule.categories,
-  };
-}
-
-function mapDispatchToProps(dispatch: ThunkDispatch<RootState, void, Action>) {
-  return {
-    fetchCoins: (vsCurrency: string, page = 1, category?: string) =>
-      dispatch(fetchCoinsPerMarket(vsCurrency, page, category)),
-    fetchCategories: () => dispatch(fetchCoinsCategories()),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(CoinsList);
+export default CoinsList;
