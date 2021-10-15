1116e79451579eb46946422d91a006e8add571ac
Firmino Changani <firmino.changani@gmail.com>
2021-10-15 22:17:54 +0100

    refactor(#33): remove localStorage-based persistence layer

--- src/modules/exchanges/store/exchanges.thunks.ts
+++ src/modules/exchanges/store/exchanges.thunks.ts
@@ -1,19 +1,13 @@
 import axios from "axios";
 import { AppThunk } from "@store/store.types";
+import { getRequestOptions } from "@common/utils";
 import { Exchange, FullExchange } from "./exchanges.interface";
 import { SET_ERROR, SET_EXCHANGE, SET_EXCHANGES } from "./exchanges.constants";
-import { getCachedData, getRequestOptions, isDataCached, setCache } from "@common/utils";
 
 export const getExchanges = (page = 1, perPage = 50): AppThunk => {
   return async (dispatch) => {
-    const key = "cg.exchanges";
     try {
-      if (isDataCached(key)) {
-        return dispatch({ type: SET_EXCHANGES, payload: getCachedData<Exchange>(key) });
-      }
-
       const { data } = await axios.get<Exchange[]>("/exchanges", getRequestOptions(page, perPage));
-      setCache(key, data);
       dispatch({ type: SET_EXCHANGES, payload: data });
     } catch (error) {
       dispatch({ type: SET_ERROR, payload: error });
@@ -24,13 +18,7 @@ export const getExchanges = (page = 1, perPage = 50): AppThunk => {
 export const getSingleExchange = (id: string): AppThunk => {
   return async (dispatch) => {
     try {
-      const key = `cg.exchanges.${id}`;
-      if (isDataCached(key)) {
-        return dispatch({ type: SET_EXCHANGE, payload: getCachedData(key) });
-      }
-
       const { data } = await axios.get<FullExchange>(`/exchanges/${id}`);
-      setCache(key, data);
       dispatch({ type: SET_EXCHANGE, payload: data });
     } catch (error) {
       dispatch({ type: SET_ERROR, payload: error });
