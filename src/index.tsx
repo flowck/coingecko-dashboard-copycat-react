9e82dbee189e96864fa80f64c2d6a243665977b7
Firmino Changani <firmino.changani@gmail.com>
2021-10-15 22:33:09 +0100

    refactor(#33): setup redux-persist
    - set sessionStorage as storage engine

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import App from "./App";
 import React from "react";
 import axios from "axios";
-import store from "./store";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { store, persistor } from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 axios.defaults.baseURL = process.env.REACT_APP_COINGECKO_API;
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App></App>
-    </Router>
+    <PersistGate loading={null} persistor={persistor}>
+      <Router>
+        <App></App>
+      </Router>
+    </PersistGate>
   </Provider>,
   document.querySelector("#root")
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,15 @@
 import thunk from "redux-thunk";
 import logger from "redux-logger";
 import { coinsReducer } from "../modules/coins/store";
+import { persistStore, persistReducer } from "redux-persist";
+import sessionStorage from "redux-persist/lib/storage/session";
 import { compose, createStore, applyMiddleware, combineReducers } from "redux";
 import { exchangesReducer } from "./../modules/exchanges/store/exchanges.reducer";
 
+const persistConfig = {
+  key: "cg",
+  storage: sessionStorage,
+};
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 /**
@@ -13,8 +19,8 @@ const reducers = combineReducers({
   coinsModule: coinsReducer,
   exchangesModule: exchangesReducer,
 });
+const persistedReducer = persistReducer(persistConfig, reducers);
 
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk, logger)));
-
-export default store;
+export const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk, logger)));
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
